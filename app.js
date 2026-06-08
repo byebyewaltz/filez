5c2570308abf02701fce1c601deda4f397962d8e
Donna Chen <donnaxtc@gmail.com>
2026-06-08 00:57:43 -0400

    completed

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 import express from "express";
-import filesRouter   from "./routes/files.js";
-import foldersRouter from "./routes/folders.js";
+import filesRouter from "#api/files";
+import foldersRouter from "#api/folders";
 
 const app = express();
 export default app;
 
-// Body-parsing middleware
+// CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+// logging
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
+
+// body parsing
 app.use(express.json());
 
-// Routes
-app.use("/files",   filesRouter);
+// routes
+app.use("/files", filesRouter);
 app.use("/folders", foldersRouter);
 
-// Error-handling middleware — must be last, must have 4 params
+// error handling
 app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send("Internal server error.");
+  res.status(500).send("Sorry! Something went wrong :(");
 });
--- db/seed.js
+++ db/seed.js
@@ -1,18 +1,25 @@
 import db from "#db/client";
-
-import { createFile } from "#db/queries/files";
 import { createFolder } from "#db/queries/folders";
+import { createFile } from "#db/queries/files";
+import { faker } from "@faker-js/faker"
 
 await db.connect();
 await seed();
 await db.end();
 console.log("🌱 Database seeded.");
 
+
+// create the folders - 3 folders and had to create files 5 files per folder 
 async function seed() {
-  for (let i = 1; i <= 3; i++) {
-    const folder = await createFolder("Folder " + i);
-    for (let j = 1; j <= 5; j++) {
-      await createFile("File " + j, 1000 * j, folder.id);
-    }
+  for (let i = 0; i < 3; i++) {
+  const folder = await createFolder({ name: faker.person.fullName() });
+
+  for (let i = 0; i < 5; i++) {
+    await createFile({
+      name: faker.system.fileName(),
+      size: faker.number.int({ min: 1, max: 10000 }),
+      folder_id: folder.id
+    });
   }
+}
 }
